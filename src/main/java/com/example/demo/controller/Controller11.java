5ef19582114749ab54d2623b275bd9894428d25f
0uhwu0 <dbgusdn2048@naver.com>
2023-04-13 14:51:31 +0900

    2023-04-13

---
+++ src/main/java/com/example/demo/controller/Controller11.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import java.sql.*;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/sub11")
+public class Controller11 {
+	
+	@RequestMapping("link1")
+	public void method1() {
+		// 2. business logic
+		 String sql = "SELECT CustomerNmae FROM Customers WHERE CustomerID = 1";
+		
+		// db 연결
+		Connection connection = DriverManager.getConnection(url, name, password);
+		// Statement 객체 생성
+		Statement statement = connection.createStatement();
+		// 쿼리 실행
+		// 쿼리 실행 결과 얻고
+		 ResultSet resultset = statement.executeQuery(sql);
+		// 쿼리 실행 결과 가공
+		if (resultset.next()) {
+			String name = resultset.getString("CustomerName");
+		}
+		
+	}
+}
