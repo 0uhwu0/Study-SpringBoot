09eef3fbc5032039b83e93057e5a6a93458e487f
0uhwu0 <dbgusdn2048@naver.com>
2023-04-18 17:26:26 +0900

    2023-04-18

--- src/main/java/com/example/demo/domain/Supplier.java
+++ src/main/java/com/example/demo/domain/Supplier.java
@@ -3,12 +3,13 @@ package com.example.demo.domain;
 import lombok.*;
 
 @Data
-public class Customer {
+public class Supplier {
 	private int id;
 	private String name;
 	private String contactName;
 	private String address;
 	private String city;
-	private String postalCode;
 	private String country;
+	private String postalCode;
+	private String phone;
 }
